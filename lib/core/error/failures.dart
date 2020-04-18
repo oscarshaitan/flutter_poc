eadc9788097d869b24c5262a48a37cf1e606e6df
otigreros <otigreros@condorlabs.io>
2020-04-18 17:23:48 -0500

    data sources created

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -4,3 +4,10 @@ abstract class Failure extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+// General failures
+
+class ServerFailure extends Failure {}
+
+class CacheFailure extends Failure {}
+
---
+++ test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fluttertest/core/platform/network_info.dart';
+import 'package:fluttertest/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:fluttertest/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:fluttertest/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'package:mockito/mockito.dart';
+
+class MockRemoteDataSource extends Mock
+    implements NumberTriviaRemoteDataSource {}
+
+class MockLocalDataSource extends Mock implements NumberTriviaLocalDataSource {}
+
+class MockNetworkInfo extends Mock implements NetworkInfo {}
+
+void main() {
+  NumberTriviaRepositoryImpl repositoryImpl;
+  MockRemoteDataSource mockRemoteDataSource;
+  MockLocalDataSource mockLocalDataSource;
+  MockNetworkInfo mockNetworkInfo;
+
+  setUp(() {
+    mockRemoteDataSource = MockRemoteDataSource();
+    mockLocalDataSource = MockLocalDataSource();
+    mockNetworkInfo = MockNetworkInfo();
+    repositoryImpl = NumberTriviaRepositoryImpl(
+        remoteDataSource: mockRemoteDataSource,
+        localDataSource: mockLocalDataSource,
+        networkInfo: mockNetworkInfo);
+  });
+}
